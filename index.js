5b2070f7dbcdc8cbbd53264f825c4c1429c0e229
CHONDO DIO <you@example.com>
2024-04-04 21:53:23 +0300

    production

--- index.js
+++ index.js
@@ -30,7 +30,22 @@ navlist.addEventListener("click", (event) => {
     event.stopPropagation();
 });
 
+  // swiper home and gallary
 
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop: true,
+  });
+  
 
 
 
